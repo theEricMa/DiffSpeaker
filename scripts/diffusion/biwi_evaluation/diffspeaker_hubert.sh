0724e2a6b0004a5a1767c5f8e4b24f6ce50b05e6
theEricMa <zm2354@columbia.edu>
2024-01-31 04:57:11 +0000

    init_update

---
+++ scripts/diffusion/biwi_evaluation/diffspeaker_hubert.sh
@@ -0,0 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0
+python eval_biwi.py \
+    --cfg configs/diffusion/biwi/diffspeaker_hubert.yaml \
+    --cfg_assets configs/assets/biwi.yaml \
---
+++ scripts/diffusion/biwi_evaluation/diffspeaker_wav2vec2.sh
@@ -0,0 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0
+python eval_biwi.py \
+    --cfg configs/diffusion/biwi/diffspeaker_wav2vec2.yaml \
+    --cfg_assets configs/assets/biwi.yaml \
