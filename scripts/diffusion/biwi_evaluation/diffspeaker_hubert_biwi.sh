d9e91d023c9d98704fb698dfbc2a9a12a0eb6810
theEricMa <zm2354@columbia.edu>
2024-01-31 05:12:55 +0000

    init_update

--- scripts/diffusion/biwi_evaluation/diffspeaker_hubert_biwi.sh
+++ scripts/diffusion/biwi_evaluation/diffspeaker_hubert_biwi.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
 python eval_biwi.py \
-    --cfg configs/diffusion/biwi/diffspeaker_hubert.yaml \
+    --cfg configs/diffusion/biwi/diffspeaker_hubert_biwi.yaml \
     --cfg_assets configs/assets/biwi.yaml \
--- scripts/diffusion/biwi_evaluation/diffspeaker_wav2vec2_biwi.sh
+++ scripts/diffusion/biwi_evaluation/diffspeaker_wav2vec2_biwi.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
 python eval_biwi.py \
-    --cfg configs/diffusion/biwi/diffspeaker_wav2vec2.yaml \
+    --cfg configs/diffusion/biwi/diffspeaker_wav2vec2_biwi.yaml \
     --cfg_assets configs/assets/biwi.yaml \
