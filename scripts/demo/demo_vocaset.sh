28a6bb0603e1bf512bf9eccb20559c5a28f11039
theEricMa <zm2354@columbia.edu>
2024-02-07 07:07:37 +0000

    init_update

--- scripts/demo/demo_vocaset.sh
+++ scripts/demo/demo_vocaset.sh
@@ -1,22 +1,22 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
-# use hubert backbone
-python demo_vocaset.py \
-    --cfg configs/diffusion/vocaset/diffspeaker_hubert_vocaset.yaml \
-    --cfg_assets configs/assets/vocaset.yaml \
-    --template datasets/vocaset/templates.pkl \
-    --example demo/wavs/speech_obama.wav \
-    --ply datasets/vocaset/templates/FLAME_sample.ply \
-    --checkpoint checkpoints/vocaset/diffspeaker_hubert_vocaset.ckpt \
-    --id FaceTalk_170809_00138_TA
-
-# # use wav2vec2 backbone
+# # use hubert backbone
 # python demo_vocaset.py \
-#     --cfg configs/diffusion/vocaset/diffspeaker_wav2vec2_vocaset.yaml \
+#     --cfg configs/diffusion/vocaset/diffspeaker_hubert_vocaset.yaml \
 #     --cfg_assets configs/assets/vocaset.yaml \
 #     --template datasets/vocaset/templates.pkl \
-#     --example demo/wavs/speech_obama.wav \
+#     --example demo/wavs/speech_long.wav \
 #     --ply datasets/vocaset/templates/FLAME_sample.ply \
-#     --checkpoint checkpoints/vocaset/diffspeaker_wav2vec2_vocaset.ckpt \
+#     --checkpoint checkpoints/vocaset/diffspeaker_hubert_vocaset.ckpt \
 #     --id FaceTalk_170809_00138_TA
+
+# use wav2vec2 backbone
+python demo_vocaset.py \
+    --cfg configs/diffusion/vocaset/diffspeaker_wav2vec2_vocaset.yaml \
+    --cfg_assets configs/assets/vocaset.yaml \
+    --template datasets/vocaset/templates.pkl \
+    --example demo/wavs/speech_long.wav \
+    --ply datasets/vocaset/templates/FLAME_sample.ply \
+    --checkpoint checkpoints/vocaset/diffspeaker_wav2vec2_vocaset.ckpt \
+    --id FaceTalk_170809_00138_TA
 
